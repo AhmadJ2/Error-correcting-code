4440e5483244854dd71a70d5c49533a75318b61a
AhmadJ2 <74082182+AhmadJ2@users.noreply.github.com>
2021-07-30 20:55:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,18 +2,14 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+sys.path.append('/usr/local//lib/python3.8/dist-packages/sympy')
 from sympy import *
 import numpy
 import random
 
 debug = False
-# inputs:
-encode = "1234567890abc"
-print("Sending: ", encode)
-# we sent 16 points, k is 4, error = 2,
-# t = n - e -> n = t + e
-print('Number of errors that will occur when sending the message:')
-error = int(input())
+
 
 
 # maximum error: if |field| is 28
@@ -47,17 +43,24 @@ def make_noise(X_arr, Y_arr, num_of_noise):
         if len(history) == num_of_noise:
             break
 
-
+# inputs:
+encode = "00100"
+print("Sending: ", encode)
+print('Number of errors that will occur when sending the message:')
+error = int(input())
 x_arr = []
 y_arr = []
+field = 10
 # Change the 28 to change the field size!
-for i in range(0, 28):
+for i in range(0, field):
     y_arr.append(encode_one_letter(encode, i))
     x_arr.append(i)
+print('The message was encoded to: ', [[x_arr[i],y_arr[i]] for i in range(0,field)])
 make_noise(x_arr, y_arr, error)
+print('The message after the noise tunnel: ', [[x_arr[i],y_arr[i]] for i in range(0,field)])
 n = len(x_arr)
 k = len(encode) - 1
-t = int(sqrt(2 * k * n)) - 3
+t = int(sqrt(2 * k * n)) - 1
 print('n is: ', n, '\nD is:', t, '\nt is: ', t + 1, '\ne is ', error, '\nk is: ', k)
 
 for i in range(0, list.__len__(x_arr)):
@@ -80,35 +83,22 @@ def double_sigma_algo1_last_step(D, k, co_acc, Z_arr):  # FOR BUILDING THE (1,k)
                 i = i + 1
 
 
-def can_be_poly(poly):  # TESTING IF THE MULTIVARIATE POLYNOMIAL IS IN THE FORM OF (Y-P(X))
-    if poly.count(Symbol('y')) != 1:
-        return False
-    args = poly.args
-    found_a_y = False
-    for arg in args:
-        if arg.count(Symbol('y')) == 1:
-            if arg.is_Symbol:
-                if arg == (Symbol('y')):
-                    found_a_y = True
-            if arg.is_Mul:
-                arg_ = arg.args
-                if arg_[1] == (Symbol('y')) and tuple.__len__(arg_) == 2:
-                    found_a_y = True
-    return found_a_y
-
-
 def can_be_poli_div(p):
     # TESTING IF THE MULTIVARIATE POLYNOMIAL IS IN THE FORM OF (Y-P(X)) WITH FEW ADDITIONS FOR THE RETURN VALUE
     args = p.args
+    count = 0
+    legal = 0
+    e = 1
     for arg in args:
-        if arg.count(Symbol('y')) == 1:
-            if arg.is_Symbol:
-                if arg == (Symbol('y')):
-                    return 1
-            if arg.is_Mul:
-                arg_ = arg.args
-                if arg_[1] == (Symbol('y')) and tuple.__len__(arg_) == 2 and arg_[0].is_Number:
-                    return arg_[0]
+        if (arg.replace(Symbol('y'), 1)*2 == arg.replace(Symbol('y'),2)) and arg.replace(Symbol('x'),0) == arg:
+            legal = 1
+            count = count + 1
+            if (arg.replace(Symbol('y'), 1).replace(Symbol('x'), 1) < 0):
+                e = -1
+        if (arg.replace(Symbol('y'), 1) * 2 == arg.replace(Symbol('y'), 2)) and arg.replace(Symbol('x'), 0) != arg:
+            return 'null'
+    if (count == 1) and legal == 1:
+        return e
     return 'null'
 
 #########################################################BUILDING THE Q(X,Y)#######################################################
@@ -142,8 +132,7 @@ for i in range(0, numpy.size(z_acc[0])):
 for i in range(0, numpy.size(z_acc[0])):
     if not solved.keys().__contains__(Symbol('a' + str(i))):
         poly_coeff[i] = 1
-while True: # Here we keep replacing each 'solved' unknown to further find new unknowns values, this step will work based on
-    # RS codes algorithms, thats why we use 'while True', beware, missing with 'D' values will cause this loop to never end sometimes!
+while True:
     if list.count(poly_coeff, 'null') == 0:  # all coefficients found
         break
     for i in range(0, numpy.size(z_acc[0])):  # going through the coefficients to try and replace them
@@ -166,67 +155,65 @@ for i in range(0, list.__len__(x_arr)):
         print((ff_arr_flat.replace(Symbol('x'), x_arr[i]).replace(Symbol('y'), y_arr[i])))
 
 #########################################################################ANALYZING THE OUTPUT########################################################################
-
 print('-------')
 f = False
 
 # Here we split the Q(X,Y) into smaller polynomials, we study each one of them and see
 # if it is in the form of "Y-P(X)", and see if it agrees with atleast 't' coordinates
 # and it is a poly with at most 'k' degree.
-if not (ff_arr_flat.__eq__(factor(ff_arr_flat))):
+if ff_arr_flat.__eq__(factor(ff_arr_flat)):
+    ff_arr_flat = (ff_arr_flat)
+print('Q(X,Y): ', ff_arr_flat)
+print('Factorized Q(X,Y): ', factor(ff_arr_flat))
+c = 0
+skip = 0
+for x in factor(ff_arr_flat).args:
     c = 0
-    skip = 0
-    for x in factor(ff_arr_flat).args:
-        if debug:
-            print('DEBUG: one of the poly in the BIG poly is :', x)
-        p = can_be_poli_div(x)
-        if p != 'null':
-            for i in range(0, list.__len__(x_arr)):
-                if int(x.replace(Symbol('x'), x_arr[i]).replace(Symbol('y'), y_arr[i])) == 0:
-                    c = c + 1
-            if c > t:
-                if not x.is_Number:
-                    x = x.replace(Symbol('y'), 0)
-                    x_ = 0 - (x * p)
-                    ret = ""
-                    count = 0
-                    index = 0
-                    map = {}
-                    while index < len(x_.args):
-                        if x_.args[index].is_Number:
-                            map[0] = x_.args[index]
-                        elif len(x_.args[index].args) == 0:
-                            map[1] = x_.args[index].replace(Symbol('x'), 1)
-                        elif len(x_.args[index].args[1].args) < 2 and x_.args[index].args[0].is_Number:
-                            map[1] = x_.args[index].args[0]
-                        elif x_.args[index].args[1].is_Number:
-                            map[index] = 1
-                        else:
-                            map[x_.args[index].args[1].args[1]] = x_.args[index].args[0]
-                        index += 1
-                    for i in range(0, k + 1):
-                        if map.__contains__(i):
-                            if map[i] < 10:
-                                ret += chr(map[i] + ord('0'))
-                            else:
-                                ret += chr(map[i] - 10 + ord('a'))
-                        else:
-                            ret += '0'
-                    print("Receiving: ", ret)
-                    f = True
-                    break
-            c = 0
-else:
-    c = 0
-    if can_be_poly(ff_arr_flat):
+    p = can_be_poli_div(x)
+    if p != 'null':
         for i in range(0, list.__len__(x_arr)):
-            if int(ff_arr_flat.replace(Symbol('x'), x_arr[i]).replace(Symbol('y'), y_arr[i])) == 0:
+            if int(x.replace(Symbol('x'), x_arr[i]).replace(Symbol('y'), y_arr[i])) == 0:
                 c = c + 1
         if c > t:
-            if not ff_arr_flat.is_Number:
-                div = can_be_poli_div(ff_arr_flat)
-                x = ff_arr_flat.replace(Symbol('y'), 0)
-                x_ = 0 - (x * div)
-                print('Found one factor of Q(X,Y) that we can use: ', x_)
+            if not x.is_Number:
+                print('x is ',c)
+                x = x.replace(Symbol('y'), 0)
+                x_ = 0 - (x * p)
+                ret = ""
+                count = 0
+                index = 0
+                map = {}
+                while index < len(x_.args) and x_.is_Add:
+                    if x_.args[index].is_Number:
+                        map[0] = abs(x_.args[index])
+                    elif len(x_.args[index].args) == 0:
+                        map[1] = abs(x_.args[index].replace(Symbol('x'), 1))
+                    elif len(x_.args[index].args[1].args) < 2 and x_.args[index].args[0].is_Number:
+                        map[1] = abs(x_.args[index].args[0])
+                    elif x_.args[index].args[1].is_Number:
+                        map[x_.args[index].args[1]] = 1
+                    else:
+                        map[x_.args[index].args[1].args[1]] = x_.args[index].args[0]
+                    index += 1
+                if x_.is_Number:
+                    map[0]=x_
+                if x_.is_Symbol:
+                    map[1] = 1
+                if x_.args[0].is_Number:
+                    map[x_.args[1].args[1]] = abs(x_.args[0])
+                if x_.args[1].is_Number:
+                    map[x_.args[1]] = 1
+                for i in range(0, k + 1):
+                    if map.__contains__(i):
+                        if map[i] < 10:
+                            ret += chr(map[i] + ord('0'))
+                        else:
+                            ret += chr(map[i] - 10 + ord('a'))
+                    else:
+                        ret += '0'
+                print("Decoded into: ", ret)
+                f = True
+                break
+        c = 0
 if not f:
     print('could not find a polynomial that satisfies both \'k\' and \'t\' restrictions ')
